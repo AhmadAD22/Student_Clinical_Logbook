37bd6aee31eb13b0fac8f0ea5d2a3e574e0afb45
unknown <ASUS@client.Al-Baath.com>
2023-04-06 03:47:03 -0600

    supervisor

--- evaluating/action/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Action,ActionInformation
-
-
-admin.site.register(Action)
-admin.site.register(ActionInformation)
--- evaluating/student/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('',views.StudentViewset.as_view({'get':'list'})),
-    path('byyear/<int:academic_year>',views.StudentViewset.as_view({'get':'listbyclass'})),
-    path('<int:pk>',views.DetaledStudentMixins.as_view(),name='mds'),
-]
